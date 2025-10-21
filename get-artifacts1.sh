2b3ac98390f2b2878a328d88cb83b03e498c9f04
merketom <tom.merkel@univ-rouen.fr>
2025-10-21 17:24:35 +0200

    API.V1

---
+++ get-artifacts1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer <YOUR-TOKEN>" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/OWNER/REPO/actions/artifacts
